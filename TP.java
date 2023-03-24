352c3fc66a4ca60c13101913b06e7a1d5db968d7
mye <mye@mye-ASUS-PC.mshome.net>
2023-03-23 23:57:56 -0300

    Editando TP.java desde NetBeans c/ssh.

--- TP.java
+++ TP.java
@@ -6,5 +6,6 @@ public class TP {
     public static void main(String[] args) {
         System.out.println("Hola grupo 6");
         System.out.println("Hola !!!");
+        System.out.println("Cambio desde NetBeans");
     }
 }
