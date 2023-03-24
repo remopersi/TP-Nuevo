bd5d136ba714ecc3120a30630e44cfec02afc7f8
alezekone <>
2023-03-24 00:15:15 -0300

    Editando TP.java desde NetBeans c/ssh.

--- TP.java
+++ TP.java
@@ -7,5 +7,6 @@ public class TP {
         System.out.println("Hola grupo 6");
         System.out.println("Hola !!!");
         System.out.println("Cambio desde NetBeans.");
+        System.out.println("Por qué no muestra a Pusheen?");
     }
 }
